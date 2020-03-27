0a04800a57e358893b171ac199016890e6d96003
Godwin Idemudia <godwinide@gmail.com>
2020-03-27 10:06:11 -0700

    create admin

--- config/auth.js
+++ config/auth.js
@@ -1,12 +1,12 @@
 module.exports = {
     ensureAuthenticated: function(req, res, next) {
-      if (!req.isAuthenticated()) {
+      if (req.isAuthenticated()) {
         return next();
       }
       res.redirect('/admin/login');
     },
     forwardAuthenticated: function(req, res, next) {
-      if (req.isAuthenticated()) {
+      if (!req.isAuthenticated()) {
         return next();
       }
       res.redirect('/');      
