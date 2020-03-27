f25b860780de80e28e652ccdb1d98b8d591631a5
Godwin Idemudia <godwinide@gmail.com>
2020-03-27 08:42:27 -0700

    first commit

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: "mongodb+srv://Godwin:godwin222sm@cluster0-q4j31.mongodb.net/test?retryWrites=true&w=majority" 
+    mongoURI: "mongodb://localhost/posProject" 
 };
