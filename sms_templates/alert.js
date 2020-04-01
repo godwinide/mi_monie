d635d28231d0155cebb3dc7ee8a1830ad3b8e96e
Godwin Idemudia <godwinide@gmail.com>
2020-03-31 18:16:18 -0700

    added sms api

--- sms_templates/alert.js
+++ sms_templates/alert.js
@@ -12,7 +12,7 @@ module.exports = function (account, amount, trans_type, transaction){
         Teller_ID: ${transaction.teller_ID}
         Value Date: ${new Date().toLocaleDateString()}`
 
-        axios(`https://portal.nigeriabulksms.com/api/?username=enaland39@gmail.com&password=doxabet2020&message=${body}&sender=mymonie&verbose=true&mobiles=${account.phone_number}`)
+        axios(`https://portal.nigeriabulksms.com/api/?username=enaland39@gmail.com&password=doxabet2020&message=${body}&sender=mymonie&verbose=true&mobiles=234${String(account.phone_number).slice(1)}`)
         .then(res => {
             console.log(res.data);
         })
--- sms_templates/welcome.js
+++ sms_templates/welcome.js
@@ -8,7 +8,7 @@ module.exports = function (account, pin){
         Your login password/pin: ${pin}
         Account Balance: ${account.account_number}`;
 
-        axios(`https://portal.nigeriabulksms.com/api/?username=enaland39@gmail.com&password=doxabet2020&message=${body}&sender=mymonie&verbose=true&mobiles=${account.phone_number}`)
+        axios(`https://portal.nigeriabulksms.com/api/?username=enaland39@gmail.com&password=doxabet2020&message=${body}&sender=mymonie&verbose=true&mobiles=234${String(account.phone_number).slice(1)}`)
         .then(res => {
             console.log(res.data);
         })
