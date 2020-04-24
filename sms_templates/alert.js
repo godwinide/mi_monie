160d7b1cf0d408d1fd04c892fa1900fcabfca1f1
Godwin Idemudia <godwinide@gmail.com>
2020-04-24 07:55:50 -0700

    error fiex

--- sms_templates/alert.js
+++ sms_templates/alert.js
@@ -4,7 +4,7 @@ const padMinutes = require("../utils/padMinutes");
 module.exports = function (account, amount, trans_type, transaction){
     const body = `Dear ${account.account_number}, your account has been ${transaction.transaction_type}ed 
     with ${amount}. Account Balance: ${transaction.available_balance}.`;
-    axios(`https://portal.nigeriabulksms.com/api/?username=enaland39@gmail.com&password=doxabet2020&message=${body}&sender=EnalandGroup&verbose=true&mobiles=234${String(account.phone_number).slice(1)}`)
+    axios(`https://portal.nigeriabulksms.com/api/?username=enaland39@gmail.com&password=doxabet2020&message=${body}&sender=ENLBS&verbose=true&mobiles=234${String(account.phone_number).slice(1)}`)
     .then(res => {
         console.log(res.data);
     })
